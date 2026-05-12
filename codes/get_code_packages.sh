bad2e4f7266ab2e5b17c64a209f6ead7211c0313
Naveenkumar Senthilkumar <naveenkumar75388@gmail.com>
2026-05-12 14:04:08 -0400

    changed music branch from chun_dev to main

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -22,7 +22,7 @@ rm -fr kompost_code/.git
 
 # download MUSIC
 rm -fr MUSIC_code
-git clone --depth=3 https://github.com/MUSIC-fluid/MUSIC -b chun_dev MUSIC_code
+git clone --depth=3 https://github.com/MUSIC-fluid/MUSIC -b main MUSIC_code
 (cd MUSIC_code; git checkout e25e49363e92c269f61fe7b8ab4bf5368bcc6d23)
 rm -fr MUSIC_code/.git
 
